4103ba03526807c7f5ea8c6efaf5e5b138cd8ae3
OuniAbir <ouniabir118@gmail.com>
2022-01-21 10:25:55 +0100

    Backend - project creation

---
+++ Backend/src/test/java/com/programming/redditClone/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.redditClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
